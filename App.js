b8156509429c3e0bd0e499fa09ae8e0fe140daf6
Wendy <wendysaules911230@gmail.com>
2023-05-05 12:47:33 -0500

    inicio de los pods

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
+import { useEffect, useMemo, useState } from 'react';
+import { useColorScheme } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider } from 'styled-components/native';
+import { DefaultTheme, DarkTheme } from './src/theme';
+import { NavigationContainer } from '@react-navigation/native';
+import RouteApp from './src/theme/RouteApp';
 export default function App() {
+  const [theme, setTheme] = useState();
+  const colorScheme = useColorScheme();
+
+  // const theme = useMemo(() => {
+  //   if (!colorScheme) return DefaultTheme;
+  //   return colorScheme === 'dark' ? DarkTheme : DarkTheme;
+  // }, [colorScheme]);
+
+  useEffect(() => {
+    setTheme(DarkTheme)
+  }, [colorScheme])
+
+  console.log(theme, "theme")
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <StatusBar style='dark' />
+        <NavigationContainer theme={theme}>
+          <RouteApp />
+        </NavigationContainer>
+        <View style={styles.container}>
+          <Text style={{ color: "red" }}>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/theme/RouteApp/RouteApp.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from 'styled-components/native';
+// import {
+//     CardStyleInterpolators,
+//     createStackNavigator,
+//     HeaderStyleInterpolators,
+// } from '@react-navigation/stack';
+
+//const Stack = createStackNavigator <RootStackParamList>();
+
+const RouteApp = () => {
+    const theme = useContext(ThemeContext);
+    return ( <></> );
+}
+
+export default RouteApp;
